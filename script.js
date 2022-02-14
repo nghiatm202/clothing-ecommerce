995583e487dcb031d063bc5229ecf691a27adb06
Tran Minh Nghia <nghiatm202@gmail.com>
2022-02-14 21:13:53 +0700

    max-width: 799px

---
+++ script.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById('bar')
+const navbar = document.getElementById('navbar')
+const close = document.getElementById('close')
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    navbar.classList.add('active')
+  })
+}
+
+if (close) {
+  close.addEventListener('click', () => {
+    navbar.classList.remove('active')
+  })
+}
